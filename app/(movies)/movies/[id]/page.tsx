4df0411a0240c949e5ebbf24715136333537c4dd
hanaru <133945249+1roo@users.noreply.github.com>
2024-06-20 13:35:32 +0900

    페이지 꾸미기 및 동적라우팅

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     //둘 중 먼저 완료되는 것부터 로드됨~ Suspense는 로딩 분리(await)하는 컴포넌트
     <div>
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,27 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("뭔가 이상해..");
-  //   const response = await fetch(`${API_URL}/${id}/videos`);
-  //   return response.json();
+  // throw new Error("뭔가 이상해..");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://www.youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-white; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
